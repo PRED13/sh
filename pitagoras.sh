2fc655463888a8752a156bff2d53ed5bcd53a751
PRED13 <155868501+PRED13@users.noreply.github.com>
2025-05-25 23:15:50 -0600

    Add files via upload

---
+++ pitagoras.sh
@@ -0,0 +1,21 @@
+echo "dame un numero"
+read x
+echo "ingresa otro numero"
+read y
+res=x*x
+c=1
+m=1
+echo "la evaluacion del numero es "
+a=1
+if [a -gt x]
+if [c -gt x]
+if [m -gt x]
+then [x \* x] + [m \* m]=res
+ac2= m * m
+echo $m "*" $m "=" ac2
+ac3= x * x
+m=m+1
+x= x+1
+m=1
+echo "los numeros sumados al cuadrodo son" $res "son" $m "y" $x
+fi
